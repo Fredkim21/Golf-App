7cb09da02f313b68e2c1e0af9160f029aded6f7a
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2023-03-01 15:56:44 -0800

    broken webpack

--- client/webpack.config.client.js
+++ client/webpack.config.client.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  // mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
